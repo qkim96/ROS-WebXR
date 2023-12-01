0e4b114031dc5adc7b4f3f8d70dd38419cb76d69
Kyu <kkr0913@gmail.com>
2023-12-01 05:08:05 -0500

    delete

--- src/ws/build/turtlebot3_simulations/turtlebot3_fake/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kyu/Documents/ROS-WebXR/src/ws/src/turtlebot3_simulations/turtlebot3_fake
-# Build directory: /home/kyu/Documents/ROS-WebXR/src/ws/build/turtlebot3_simulations/turtlebot3_fake
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
