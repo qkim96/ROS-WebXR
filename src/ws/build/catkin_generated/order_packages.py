a901010465a28f97cfe8313cfc21fde47852f283
Kyu <kkr0913@gmail.com>
2023-12-01 05:11:49 -0500

    test index.html for AR+ROS on same server

---
+++ src/ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/kyu/Documents/ROS-WebXR/src/ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/kyu/Documents/ROS-WebXR/src/ws/devel;/opt/ros/noetic'.split(';') if '/home/kyu/Documents/ROS-WebXR/src/ws/devel;/opt/ros/noetic' != '' else []
---
+++ src/ws/build/turtlebot3/turtlebot3_example/CMakeFiles/turtlebot3_example_generate_messages_eus.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "/home/kyu/Documents/ROS-WebXR/src/ws/devel/share/roseus/ros/turtlebot3_example/manifest.l"
+  "/home/kyu/Documents/ROS-WebXR/src/ws/devel/share/roseus/ros/turtlebot3_example/msg/Turtlebot3Action.l"
+  "/home/kyu/Documents/ROS-WebXR/src/ws/devel/share/roseus/ros/turtlebot3_example/msg/Turtlebot3ActionFeedback.l"
+  "/home/kyu/Documents/ROS-WebXR/src/ws/devel/share/roseus/ros/turtlebot3_example/msg/Turtlebot3ActionGoal.l"
+  "/home/kyu/Documents/ROS-WebXR/src/ws/devel/share/roseus/ros/turtlebot3_example/msg/Turtlebot3ActionResult.l"
+  "/home/kyu/Documents/ROS-WebXR/src/ws/devel/share/roseus/ros/turtlebot3_example/msg/Turtlebot3Feedback.l"
+  "/home/kyu/Documents/ROS-WebXR/src/ws/devel/share/roseus/ros/turtlebot3_example/msg/Turtlebot3Goal.l"
+  "/home/kyu/Documents/ROS-WebXR/src/ws/devel/share/roseus/ros/turtlebot3_example/msg/Turtlebot3Result.l"
+  "CMakeFiles/turtlebot3_example_generate_messages_eus"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/turtlebot3_example_generate_messages_eus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
