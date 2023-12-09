48991386a11a8792b88f827269e8b9251fccac36
Kyu <kkr0913@gmail.com>
2023-12-09 15:35:17 -0500

    launch files

---
+++ src/ws/build/sim_test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kyu/Documents/ROS-WebXR/src/ws/src/sim_test
+# Build directory: /home/kyu/Documents/ROS-WebXR/src/ws/build/sim_test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
